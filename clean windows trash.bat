f1ac45f38c1eb7e9056ded631a2e4695a3917433
pham the minh <phamtheminh2409@gmail.com>
2024-10-18 15:27:58 +0700

    Update clean windows trash.bat

--- clean windows trash.bat
+++ clean windows trash.bat
@@ -1,15 +1,15 @@
 @kaitianquan
-Kinkyoku 0C (Priest of Obayashi)
-del /f /s /q %systemdrive%\*.tmp
-del /f /s /q %systemdrive%\*._mp
-del /f /s /q %systemdrive%\*.log del /f /s /q %systemdrive%\*.gid
-del /f /s /q %systemdrive%\*.chk
-del /f /s /q %systemdrive%\*.old del /f /s /q %systemdrive%\recycled\*.*
+0C Golden Song (Friends of Love)
+Del /f /s /q %devicesystem%\*.tmp
+del /f /s /q %pengpengku shisutemu% \*._mp
+Delete shisutemu%\*.gid
+del /f /s /q %devicesystem%\*.chk
+del /f /s /q %systemdrive% \*.old del /f /s /q %systemdrive% \recycle\*.*
 /f /s /q %windir%\*.bak
-del /f /s /q %windir%\prefetch\*.* rd /s /q %windir%\temp & md %windir%\temp
-del /f /q %userprofile%\cookies\*.*
-del /f /q %userprofile%\ max \*.*
-del /f /s /q “%userprofile%\Device settings\Temp\*.*” del /f /s /q “%userprofile%\Device settings\Temp\*.*”
+del /f /s /q %Windir% \prefetch\*.* rd /s /q %Windir% \temp & md %Windir% \temp
+del /f /q %User statement% \cookies\*.*
+del /f /q %Use profit % \max \*.*
+del /f /s /q "%SetPhrase%\Configuration\Temp\*.*" del /f /s /q "%SetPhrase%\Customization\Temp\*.*"
 del /f /s /q "%userprofile%\max\*.*"
-Service request completed successfully. Thank you for using PTM Soft Body.
-Temporarily closed
+PTM method.
+Rest time
